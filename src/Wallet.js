dab2826ab859016ed4f26a1dd3cf788cd2bb29a3
F0171903 Adriano Heller Mylla <adrianohellermylla@bb.com.br>
2020-08-01 23:00:44 -0400

    update

--- src/Wallet.js
+++ src/Wallet.js
@@ -5,7 +5,7 @@ class Wallet{
     this.combo = combo || false
     this.discount = this.combo !== null && this.combo ? true : false;
     this.discount ? this.discountType = ['simple','double','full'] : false;
-    this.discountType === 'simple' ? this.planPrice *= 0.1 : this.discountType === 'double' ? this.planPrice *= 0.2 : this.planPrice *= 0.3    	  
+    this.discountType === 'simple' ? this.planPrice *= 0.1 : this.discountType === 'double' ? this.planPrice *= 0.2 : this.discountType === 'full' ? this.planPrice *= 0.3 : this.planPrice    	  
   }
 
   get indexes(){
@@ -30,6 +30,17 @@ class Wallet{
     return index
   }
 
+  addPlan(index,data){
+      let planData = [];
+      planData.push(index.plan[0])	  
+      index = typeof index === 'object' && index.plan instanceof Array ? index : false;
+      data = data instanceof Object && Object.keys(data).includes('name','price') ?
+		  data : false;
+      planData.push(data)
+      index['plan'] = planData
+      return index        
+  }
+
   mountWalletPrice(data){
     let mountedPrice;
     data = typeof data === 'object' && data.plan instanceof Array ? (
@@ -40,12 +51,11 @@ class Wallet{
 }
 
 
-const buy = new Wallet('unimed-flex-2',350,true)
+const buy = new Wallet('unimed-flex-2',350,false)
 const newBuy = new Wallet('unimed-familia-3',1200,false)
 
-console.log(buy)
-console.log(newBuy)
-console.log(buy.indexes)
-console.log(buy.mountWalletPrice(buy.indexes))
 console.log(buy.generatePrice(buy.indexes))
+buy.addPlan(buy.indexes,{ name: 'odontoprev-fx-12', price: 250})
+console.log(buy.indexes)
+
 module.exports = Wallet;
